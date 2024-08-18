cc4d24b674c4441b74071e4453e6b0b1b3b70f96
insaf-badri <insafbadri7@gmail.com>
2024-08-18 04:48:52 +0100

    storing data locally

--- Amazon_ETL.py
+++ Amazon_ETL.py
@@ -33,11 +33,11 @@ def get_price(soup):
  
     return price
 
-#Function to extract Dicription
+#Function to extract Description
 def get_description(soup): 
 
     try : 
-        description =soup.find ( "ul", attrs={'class':'a-unordered-list a-vertical a-spacing-mini'}).text.strip()
+        description =soup.find ("ul", attrs={'class':'a-unordered-list a-vertical a-spacing-mini'}).text.strip()
           
     except AttributeError : 
 
@@ -82,49 +82,48 @@ def get_availability(soup):
 	return available	
 
 
-HEADERS = ({'User-Agent':
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-            'Accept-Language': 'en-US, en;q=0.5'})
+if __name__ == '__main__':
+    HEADERS = ({'User-Agent':
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+                'Accept-Language': 'en-US, en;q=0.5'})
 
-url = "https://www.amazon.com/Apple-iPhone-13-Mini-Starlight/dp/B09LKYXHK5/ref=sr_1_8?crid=3UROYK1HVSI20&dib=eyJ2IjoiMSJ9.Ln-hU6FflqpQLC0k4dwhS57oJiCuXnXZWioAtc01zy9U7Gkt5ptMbX29-x-uLsLBiqFm9KIBSEuf8Tvk61MOJYc1kcA4rQh0J4ZAonHsUhAL5w-Y0P7V5lJARVSpzJNGqif4AP4R6-qmduPlDQnt_4-LQJHh2xUchWRpsZ526szytfugo2i2VKdYALRyG6oNWECHwAVvc1tfe0ZquMABi0Q3tBYhIkJhPRPsVEp7EpY.ci1qcipmonPoyZCfp4Ix7-mj89jO0SRxc4a9_tmHkeU&dib_tag=se&keywords=iphone&qid=1723844024&sprefix=iphone%2Caps%2C249&sr=8-8&th=1"
-page = requests.get(url , headers=HEADERS)
-soup = BeautifulSoup(page.content, 'lxml')
-
-#Test one Product
-""""""
-print("Product Title =", get_title(soup))
-print("Product Price =", get_price(soup))
-print("Product Rating =", get_rating(soup))
-print("Number of Product Reviews =", get_review_count(soup))
-print("Availability =", get_availability(soup))
-print("Product Description = " , get_description(soup))
-""""""
+    URL = "https://www.amazon.com/s?k=laptop&crid=73UWUS1S13Y9&sprefix=laptop%2Caps%2C247&ref=nb_sb_noss_1"
+    page = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
 
-# Fetch links as List of Tag Objects
-links = soup.find_all("a", attrs={'class':'a-link-normal'})
+    # Fetch links as List of Tag Objects
+    links = soup.find_all("a", attrs={'class':'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
 
-links_list = []
+    links_list = []
 
-# Loop for extracting links from Tag Objects
-for link in links:
-    href = link.get('href')
-    if href and href.startswith('/'):
-    	links_list.append(href)
+    # Loop for extracting links from Tag Objects
+    for link in links:
+        href = link.get('href')
+        if href and href.startswith('/'):
+            links_list.append(href)
 
-# Loop for extracting product details from each link 
-for link in links_list:
+    # dic of list to store our data
+    d = {"title": [], "price": [], "rating": [], "count_review": [], "availability": []}
+    # to see how many links we've
+    print(f"Total links found: {len(links_list)}")
+     
+    # Loop for extracting product details from each link
+    for link in links_list:
+        new_webpage = requests.get("https://www.amazon.com" + link, headers=HEADERS)
+        new_soup = BeautifulSoup(new_webpage.content, "html.parser")
 
-	new_webpage = requests.get("https://www.amazon.com" + link, headers=HEADERS)
-	new_soup = BeautifulSoup(new_webpage.content, "html.parser")
+        d['title'].append(get_title(new_soup))
+        d['price'].append(get_price(new_soup))
+        d['rating'].append(get_rating(new_soup))
+        d['count_review'].append(get_review_count(new_soup))
+        d['availability'].append(get_availability(new_soup))
 
-	# Function calls to display all necessary product information
-	
-	print("Product Title =", get_title(new_soup))
-	print("Product Price =", get_price(new_soup))
-	print("Product Rating =", get_rating(new_soup))
-	print("Number of Product Reviews =", get_review_count(new_soup))
-	print("Availability =", get_availability(new_soup))
-	print("Product Description = " , get_description(new_soup))		
-	
-	
+    # Store our data into csv file   
+    amazon_df = pd.DataFrame.from_dict(d)
+    #Replace the empty String with NaN in titile column
+    amazon_df['title']= amazon_df['title'].replace('', np.nan)
+    #Drop rows with NaN value in title column 
+    amazon_df = amazon_df.dropna(subset=['title'])
+    # hnaya dart 2 dyal file jarabthom 
+    amazon_df.to_csv("Amazon_data.csv", header=True, index=False, sep=';')
